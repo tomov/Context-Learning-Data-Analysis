e1bedf8c97c725411166d791de0821033ee312b7
Momchil Tomov <tomov90@gmail.com>
2017-08-01 18:15:26 -0400

    peak_voxel_behavior is the new kl_weights

---
+++ peak_voxel_behavior.m
@@ -0,0 +1,50 @@
+% Correlate peak voxels from different contrasts with behavior
+%
+
+close all;
+clear all;
+
+EXPT = context_expt();
+glmodel = 151;
+
+%% Load behavior
+%
+[data, metadata] = load_data(fullfile('data', 'fmri.csv'), true, getGoodSubjects());
+goodSubjects = getGoodSubjects();
+which_rows = data.which_rows;
+
+%% Simulate behavior
+%
+prior_variance = 0.1249;
+inv_softmax_temp = 2.0064;
+params = [prior_variance inv_softmax_temp];
+which_structures = logical([1 1 1 0]);
+simulated = simulate_subjects(data, metadata, params, which_structures, which_rows, false);
+
+%% Some random voxels as controls
+%
+rand_voxels = gen_rand_voxels(fullfile('masks', 'mask.nii'), 20);
+
+%% Find the peak voxels 
+%
+[KL_structures_voxels, KL_structures_rois] = get_peak_voxels_from_contrast(EXPT, glmodel, 'KL_structures');
+[KL_weights_voxels, KL_weights_rois] = get_peak_voxels_from_contrast(EXPT, glmodel, 'KL_weights');
+
+%% Get the betas
+%
+KL_structures_betas = load_run_betas(glmodel, 'KL_structures', [KL_structures_voxels; rand_voxels]);
+KL_weights_betas = load_run_betas(glmodel, 'KL_weights', [KL_weights_voxels; rand_voxels]);
+
+%% Get the behavioral correlates
+%
+[test_liks, test_RTs] = get_test_behavior();
+
+save('results/peak_voxel_behavior_glm151.mat');
+
+%% within-subject analysis using a linear mixed effects model and/or t-tests
+%
+
+load('results/peak_voxel_behavior_glm151.mat');
+
+correlate_neural_and_behavior(KL_weights_betas, [KL_weights_rois', repmat({'random'}, 1, size(rand_voxels, 1))], test_liks, 'KL_weight betas correlated with test log likelihood: t-test');
+correlate_neural_and_behavior(KL_structures_betas, [KL_structures_rois', repmat({'random'}, 1, size(rand_voxels, 1))], test_liks, 'KL_structure betas correlated with test log likelihood: t-test');
