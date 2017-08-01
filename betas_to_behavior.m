76ee3f99241145d1843841c02323bc8dc14f9aa1
Momchil Tomov <tomov90@gmail.com>
2017-08-01 19:25:34 -0400

    betas_to_behavior like a function with voxel, sphere, and cluster args

--- betas_to_behavior.m
+++ betas_to_behavior.m
@@ -1,18 +1,31 @@
+function [means, sems, ps] = betas_to_behavior(glmodel, regressor, what)
+
 % Correlate peak voxels from different contrasts with behavior
 % Also tries with clusters of voxels (average the betas)
-% TODO dedupe with peak_voxel_behavior.m
+%
+% EXAMPLES:
+% betas_to_behavior(123, 'surprise', 'voxel')
+% betas_to_behavior(148, 'KL_weights', 'voxel')
+% betas_to_behavior(154, 'KL_structures', 'voxel')
+% betas_to_behavior(154, 'KL_weights', 'voxel')
+% betas_to_behavior(154, 'KL_structures', 'sphere')
+% betas_to_behavior(154, 'KL_weights', 'sphere')
+% betas_to_behavior(154, 'KL_structures', 'cluster')
+% betas_to_behavior(154, 'KL_weights', 'cluster')
 %
 
-close all;
-clear all;
-
 EXPT = context_expt();
-glmodel = 154;
+%glmodel = 154;
 p = 0.001;
+r = 1.814;
 direct = '+';
-regressor = 'KL_structures'; % contrasts not supported yet b/c ot load_run_betas
+%regressor = 'KL_structures'; % contrasts not supported yet b/c ot load_run_betas
+%what = 'voxel';
 
-filename = ['results/betas_to_behavior_glm', num2str(glmodel), '_', regressor];
+assert(ismember(what, {'voxel', 'sphere', 'cluster'}));
+
+filename = ['results/betas_to_behavior_glm', num2str(glmodel), '_', regressor, '_', what];
+
 
 %% Load behavior
 %
@@ -25,9 +38,6 @@ which_rows = data.which_rows;
 [V, Y, C, CI, region, extent, stat, mni, cor, results_table] = extract_clusters(EXPT, glmodel, regressor, p, direct);
 
 
-%[KL_structures_voxels, KL_structures_rois] = get_peak_voxels_from_contrast(EXPT, glmodel, 'KL_structures');
-%[KL_weights_voxels, KL_weights_rois] = get_peak_voxels_from_contrast(EXPT, glmodel, 'KL_weights');
-
 %% Get the betas
 %
 
@@ -36,35 +46,69 @@ which_rows = data.which_rows;
 %
 betas = nan(metadata.N, metadata.runsPerSubject, numel(region));
 
-for i = 1:numel(region)
-    fprintf('Getting betas for cluster t=%.3f extent=%d roi=%s peak=[%d %d %d]\n', stat(i), extent(i), region{i}, mni(i,1), mni(i,2), mni(i,3));
-    
-    x = cor(i,1);
-    y = cor(i,2);
-    z = cor(i,3);
-    assert(immse(stat(i), Y(x,y,z)) < 1e-6);
+switch what
+    case 'voxel'
+        % Take betas from peak voxels only
+        %
+        betas = load_run_betas(glmodel, regressor, mni);
+        
+        
+    case 'sphere'
+        % Average betas from a sphere around each peak voxel
+        % TODO maybe intersect with the cluster too
+        %
+        for i = 1:numel(region)
+            fprintf('Getting betas for sphere of peak voxel t=%.3f extent=%d roi=%s peak=[%d %d %d]\n', stat(i), extent(i), region{i}, mni(i,1), mni(i,2), mni(i,3));
 
-    % create mask from cluster
-    clust_idx = CI(x,y,z);
-    mask = CI == clust_idx;
-    
-    % take voxels from cluster and convert to MNI
-    voxels = find(mask);
-    assert(numel(voxels) == extent(i));
-    [x, y, z] = ind2sub(size(mask), voxels);
-    voxels = cor2mni([x, y, z], V.mat);
-    
-    % get the corresponding betas
-    clust_betas = load_run_betas(glmodel, regressor, voxels);
-    
-    % average them
-    clust_betas = mean(clust_betas, 3);
-    
-    betas(:,:,i) = clust_betas;
+            % create spherical mask around voxel
+            [~, voxels] = create_spherical_mask(mni(i,1), mni(i,2), mni(i,3), r);
+
+            % get the corresponding betas
+            sphere_betas = load_run_betas(glmodel, regressor, voxels);
+
+            % average them
+            fprintf('Averaging %d betas\n', size(sphere_betas, 3));
+            sphere_betas = mean(sphere_betas, 3);
+
+            betas(:,:,i) = sphere_betas;
+        end
+        
+        
+    case 'cluster'
+        % Average betas from the cluster of each peak voxel
+        %
+        for i = 1:numel(region)
+            fprintf('Getting betas for cluster t=%.3f extent=%d roi=%s peak=[%d %d %d]\n', stat(i), extent(i), region{i}, mni(i,1), mni(i,2), mni(i,3));
+
+            x = cor(i,1);
+            y = cor(i,2);
+            z = cor(i,3);
+            assert(immse(stat(i), Y(x,y,z)) < 1e-6);
+
+            % create mask from cluster
+            clust_idx = CI(x,y,z);
+            mask = CI == clust_idx;
+
+            % take voxels from cluster and convert to MNI
+            voxels = find(mask);
+            assert(numel(voxels) == extent(i));
+            [x, y, z] = ind2sub(size(mask), voxels);
+            voxels = cor2mni([x, y, z], V.mat);
+
+            % get the corresponding betas
+            clust_betas = load_run_betas(glmodel, regressor, voxels);
+
+            % average them
+            fprintf('Averaging %d betas\n', size(clust_betas, 3));
+            clust_betas = mean(clust_betas, 3);
+
+            betas(:,:,i) = clust_betas;
+        end
+        
+    otherwise 
+        assert(false);
 end
 
-%KL_structures_betas = load_run_betas(glmodel, 'KL_structures', [KL_structures_voxels; rand_voxels]);
-%KL_weights_betas = load_run_betas(glmodel, 'KL_weights', [KL_weights_voxels; rand_voxels]);
 
 %% Get the behavioral correlates
 %
@@ -73,9 +117,9 @@ end
 save(filename);
 fprintf('SAVING to %s\n', filename);
 
+
 %% within-subject analysis using a linear mixed effects model and/or t-tests
 %
-
 load(filename);
 
 [means, sems, ps] = correlate_neural_and_behavior(betas, region, test_liks, [regressor, ' betas correlated with test log likelihood: t-test']);
--- plot_figure.m
+++ plot_figure.m
@@ -91,4 +91,3 @@ switch plotname
         
 end
 
-
