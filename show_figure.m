bfbb2e2efeea1dea38249ac40e43f114bfd3a3b4
Momchil Tomov <tomov90@gmail.com>
2017-11-23 13:45:38 -0500

    behavior_both for figure_3

--- show_figure.m
+++ show_figure.m
@@ -309,65 +309,88 @@ switch figure_name
 
             model_means = [model_means; mean(x1c1) mean(x1c3) mean(x3c1) mean(x3c3)];
         end
-
-        % Choice probabilities for human subjects
-        %
-        human_choices = [];        
-        for condition = metadata.contextRoles
-            which_rows = data.which_rows & ~data.isTrain & strcmp(data.contextRole, condition);
-            
-            x1c1 = []; x1c3 = []; x3c1 = []; x3c3 = [];
-            for who = metadata.subjects
-                x1c1 = [x1c1, data.chose_sick(which_rows & data.cueId == 0 & data.contextId == 0 & strcmp(data.participant, who))];
-                x1c3 = [x1c3, data.chose_sick(which_rows & data.cueId == 0 & data.contextId == 2 & strcmp(data.participant, who))];
-                x3c1 = [x3c1, data.chose_sick(which_rows & data.cueId == 2 & data.contextId == 0 & strcmp(data.participant, who))];
-                x3c3 = [x3c3, data.chose_sick(which_rows & data.cueId == 2 & data.contextId == 2 & strcmp(data.participant, who))];
-            end
-            assert(isequal(size(x1c1), [metadata.runsPerContext metadata.N]));
-            assert(isequal(size(x1c3), [metadata.runsPerContext metadata.N]));
-            assert(isequal(size(x3c1), [metadata.runsPerContext metadata.N]));
-            assert(isequal(size(x3c3), [metadata.runsPerContext metadata.N]));
-            
-            human_choices = [human_choices; mean(x1c1); mean(x1c3); mean(x3c1); mean(x3c3)];
-        end
-        
-        [human_sems, human_means] = wse(human_choices');
         
         % Plot model choices probabilities
         %
         h = bar(model_means);
-        
-        % Get x-coordinates of bar centers and "flatten" the human choice
-        % data so we can overlay it nicely
-        %
-        xs = sort([h(1).XData + h(1).XOffset, ...
-                   h(2).XData + h(2).XOffset, ...
-                   h(3).XData + h(3).XOffset, ...
-                   h(4).XData + h(4).XOffset]);
-        human_means = human_means'; human_means = human_means(:);
-        human_sems = human_sems'; human_sems = human_sems(:);        
-        
-        % Plot human choices
-        %
+
         hold on;
-        errorbar(xs, human_means, human_sems, '.', 'MarkerSize', 1, 'MarkerFaceColor', [0 0 0], 'LineWidth', 1, 'Color', [0 0 0], 'AlignVertexCenters', 'off');
-        % the markers in the errorbar plot are misaligned from the error
-        % bars -- this hack adjusts them
-        xs_adjust = xs;
-        xs_adjust(1) = xs_adjust(1) + 0.0035;
-        xs_adjust(2) = xs_adjust(2) + 0.003;
-        xs_adjust(3) = xs_adjust(3) + 0.003;
-        xs_adjust(4) = xs_adjust(4) + 0.0025;
-        xs_adjust(5) = xs_adjust(5) + 0.004;
-        xs_adjust(6) = xs_adjust(6) + 0.004;
-        xs_adjust(7) = xs_adjust(7) + 0.0025;
-        xs_adjust(8) = xs_adjust(8) + 0.0025;
-        xs_adjust(9) = xs_adjust(9) + 0.003;
-        xs_adjust(10) = xs_adjust(10) + 0.004;
-        xs_adjust(11) = xs_adjust(11) + 0.003;
-        xs_adjust(12) = xs_adjust(12) + 0.0025;
-        plot(xs_adjust, human_means, 'o', 'MarkerSize', 5, 'MarkerFaceColor', [0 0 0], 'Color', [0 0 0]);
+        
+        % plot both pilot and fmri subjects' choices
+        %
+        group_x_offs = [-0.02, 0.02];
+        group_color = {[0.5 0.5 0.5], [0 0 0]};
+        for group = 1:2
+            if group == 1
+                % pilot subjects
+                [data, metadata] = load_data(fullfile('data', 'pilot.csv'), false);
+            else
+                % fmri subjects
+                [data, metadata] = load_data(fullfile('data', 'fmri.csv'), true, getGoodSubjects());
+            end
+            simulated = simulate_subjects(data, metadata, params, which_structures);        
+            
+            % Choice probabilities for human subjects
+            %
+            human_choices = [];        
+            for condition = metadata.contextRoles
+                which_rows = data.which_rows & ~data.isTrain & strcmp(data.contextRole, condition);
+
+                x1c1 = []; x1c3 = []; x3c1 = []; x3c3 = [];
+                for who = metadata.subjects
+                    x1c1 = [x1c1, data.chose_sick(which_rows & data.cueId == 0 & data.contextId == 0 & strcmp(data.participant, who))];
+                    x1c3 = [x1c3, data.chose_sick(which_rows & data.cueId == 0 & data.contextId == 2 & strcmp(data.participant, who))];
+                    x3c1 = [x3c1, data.chose_sick(which_rows & data.cueId == 2 & data.contextId == 0 & strcmp(data.participant, who))];
+                    x3c3 = [x3c3, data.chose_sick(which_rows & data.cueId == 2 & data.contextId == 2 & strcmp(data.participant, who))];
+                end
+                assert(isequal(size(x1c1), [metadata.runsPerContext metadata.N]));
+                assert(isequal(size(x1c3), [metadata.runsPerContext metadata.N]));
+                assert(isequal(size(x3c1), [metadata.runsPerContext metadata.N]));
+                assert(isequal(size(x3c3), [metadata.runsPerContext metadata.N]));
+
+                human_choices = [human_choices; mean(x1c1); mean(x1c3); mean(x3c1); mean(x3c3)];
+            end
+
+            [human_sems, human_means] = wse(human_choices');
+
+
+            % Get x-coordinates of bar centers and "flatten" the human choice
+            % data so we can overlay it nicely
+            %
+            xs = sort([h(1).XData + h(1).XOffset, ...
+                       h(2).XData + h(2).XOffset, ...
+                       h(3).XData + h(3).XOffset, ...
+                       h(4).XData + h(4).XOffset]);
+            human_means = human_means'; human_means = human_means(:);
+            human_sems = human_sems'; human_sems = human_sems(:);        
+
+            % Plot human choices
+            %
+            errorbar(xs + group_x_offs(group), human_means, human_sems, '.', 'MarkerSize', 1, 'MarkerFaceColor', group_color{group}, 'LineWidth', 1, 'Color', group_color{group}, 'AlignVertexCenters', 'off');
+            % the markers in the errorbar plot are misaligned from the error
+            % bars -- this hack adjusts them
+            xs_adjust = xs + 0.003;
+            %{
+            xs_adjust(1) = xs_adjust(1) + 0.0035;
+            xs_adjust(2) = xs_adjust(2) + 0.003;
+            xs_adjust(3) = xs_adjust(3) + 0.003;
+            xs_adjust(4) = xs_adjust(4) + 0.0025;
+            xs_adjust(5) = xs_adjust(5) + 0.004;
+            xs_adjust(6) = xs_adjust(6) + 0.004;
+            xs_adjust(7) = xs_adjust(7) + 0.0025;
+            xs_adjust(8) = xs_adjust(8) + 0.0025;
+            xs_adjust(9) = xs_adjust(9) + 0.003;
+            xs_adjust(10) = xs_adjust(10) + 0.004;
+            xs_adjust(11) = xs_adjust(11) + 0.003;
+            xs_adjust(12) = xs_adjust(12) + 0.0025;
+            %}
+            plot(xs_adjust + group_x_offs(group), human_means, 'o', 'MarkerSize', 5, 'MarkerFaceColor', group_color{group}, 'Color', group_color{group});
+        end
+        
+        
+        
         hold off;
+        
         xticklabels({'Irrelevant training', 'Modulatory training', 'Additive training'});
         ylabel('Choice probability');
         legend({'x_1c_1', 'x_1c_3', 'x_3c_1', 'x_3c_3'}, 'Position', [0.07 -0.095 1 1]);
