97a7f591be8d5fe44b2257d3479a7050a70c4380
Momchil Tomov <tomov90@gmail.com>
2018-03-22 18:00:18 -0400

    might.m wtf killing it?

--- might.m
+++ might.m
@@ -1,8 +1,17 @@
+%{
 addpath('/Users/momchil/Dropbox/Research/libs/SearchmightToolbox.Darwin_i386.0.2.5');
 
 maskfile = 'masks/mask.nii';
 event = 'feedback_onset';
 use_nosmooth = true;
+r = 2.6667;
+
+runs = 1:9; 
+trials = 6:20;
+subjs = getGoodSubjects();
+predict_what = 'condition';
+z_score = 'z-none'; 
+
 
 [mask] = load_mask(maskfile);
 % load instead
@@ -16,40 +25,32 @@ load('might/meta.mat');
 %
 [meta.voxelsToNeighbours, meta.numberOfNeighbours] = might_computeNeighborsSphere(mask, meta.colToCoord, r);
 
-% this is unnecessary -- examples == betas
-%
-%{
-betas = get_betas('masks/mask.nii', 'feedback_onset', data, metadata, false);
-[x, y, z] = ind2sub(size(mask), find(mask)); % NOTE this works ONLY b/c of the way we got the betas in ccnl_get_betas, i.e. Y(mask) == Y(find(mask))
-dimx = size(mask, 1);
-dimy = size(mask, 2); 
-dimz = size(mask, 3); 
-dimt = size(betas, 1);
-data = nan(dimx, dimy, dimz, dimt);
-for i = 1:size(betas, 2)
-    data(x(i),y(i),z(i),:) = betas(:,i);
-end
-
-[dimx,dimy,dimz,dimt] = size(data);
-nvoxels = length(meta.indicesIn3D);
-
-examples = zeros(dimt,nvoxels);
-
-for t = 1:dimt
-
-    volume = data(:,:,:,t);
-    examples(t,:) = volume(meta.indicesIn3D);
-
-end
-%}
-
 betas = get_betas(maskfile, event, data, metadata, use_nosmooth);
 dimx = size(mask, 1);
 dimy = size(mask, 2); 
 dimz = size(mask, 3); 
 nvoxels = size(betas, 2);
 
+classifier = 'gnb_searchmight'; % fast GNB
 
-%examples = f
+which_rows = data.which_rows & data.isTrain & strcmp(data.participant, 'con001');
 
+%}
+
+disp('getting inputs');
+tic
+[inputs, targets, which_rows] = classify_get_inputs_and_targets_helper(runs, trials, subjs(1), betas, predict_what, z_score, data, metadata);
+toc
+
+
+[~, labels] = max(targets, [], 2); % from one-hot vector to indices
+labelsGroup = data.runId(which_rows); % folds = runs (assumes 1 subject, and that GNB takes prior into account)
+
+classifier = 'gnb_searchmight'; % fast GNB
+
+disp('running searchmight');
+tic 
+[am,pm] = computeInformationMap(examples,labels,labelsGroup,classifier,'searchlight', ...
+                                meta.voxelsToNeighbours,meta.numberOfNeighbours);
+toc
 
