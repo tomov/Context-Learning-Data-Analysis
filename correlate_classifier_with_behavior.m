216b58231a2c6e725af37e639e936813d4289a1f
Momchil Tomov <tomov90@gmail.com>
2018-03-26 20:48:30 -0400

    correlate_classifier_with_behavior for whole-brain mask (i.e. peak best         voxel for each subject); didn't work oh well --> moving on to ROIs

--- correlate_classifier_with_behavior.m
+++ correlate_classifier_with_behavior.m
@@ -9,6 +9,8 @@
 % 6) see if that's better, on average, than the model log lik
 
 
+which_structures = logical([1 1 0 1 0]);
+[data, metadata, simulated, params] = simulate_subjects_helper(true, 'results/fit_params_results_M1M2M1_25nstarts_tau_w0.mat', 1, which_structures);
 
 
 %EXPT = context_expt();
@@ -18,12 +20,14 @@ contrast = 'KL_structures';
 event = 'trial_onset';
 r = 2.6667;
 
-method = 'cvfitcnb';
+method = 'cvfitcnb'; % gaussian naive bayes, just like searchmight 
 runs = 1:9; 
-trials = 11:20;
+trials = 6:20;
 subjs = getGoodSubjects();
 predict_what = 'condition';
 z_score = 'z-none';
+
+n_iter = 10; % how many iterations for each subject
 
 use_tmaps = false; % <-- slightly better if true; but stick with betas for consistency w/ RDMs
 use_nosmooth = true; 
@@ -57,7 +61,6 @@ max_y = max(all_y);
 min_z = min(all_z);
 max_z = max(all_z);
 
-[data, metadata] = load_data(fullfile('data', 'fmri.csv'), true, getGoodSubjects());
 %activations = get_activations(maskfile, event, data, metadata, use_nosmooth);
 
 %for i = 1:size(region, 1) % for each ROI
@@ -70,8 +73,13 @@ max_z = max(all_z);
     clust_mask = mask;
     clust_vox = find(mask);
 
+    liks_classifier = []; % avg test log lik for each run for each subject, according to classifier posterior
+    liks_model = []; % same but according to model posterior
+
     acc = [];
     for subj = subjs % for each subject
+        subject = metadata.allSubjects(subj); % 'con001' ... 'con025'
+
         filename = sprintf(file_format, event, subj, r, z_score, use_nosmooth, use_tmaps);
         [~, ~, amap] = load_mask(filename); % get accuracy map
 
@@ -79,21 +87,67 @@ max_z = max(all_z);
         [x,y,z] = ind2sub(size(clust_mask), clust_vox(j));
         fprintf('\nsubj = %d, max acc = %.4f\n', subj, amap(x,y,z));
 
+        % get sphere around peak accuracy voxel for the given cluster (same sphere as the searchmight)
+        %
         sphere_mask = create_spherical_mask_helper(mask, x, y, z, r, min_x, max_x, min_y, max_y, min_z, max_z, Vmask);
-
         sphere_activations = get_activations_submask(sphere_mask, activations);
 
+        % convert to inputs/targets
+        %
         [inputs, targets, which_rows] = classify_get_inputs_and_targets_helper(runs, trials, [subj], sphere_activations, predict_what, z_score, data, metadata);
-        inputs = inputs(:, sum(inputs,1) ~= 0); % clear columns of all 0's, previously NaNs b/c of use_nosmooth; we clear those out in might_getNeighbors()
-        [classifier, outputs, accuracy, stats] = classify_train_helper(method, inputs, targets, runs, trials, [subj], []);
+        inputs = inputs(:, sum(inputs,1) ~= 0); % clear columns of all 0's, previously NaNs b/c of use_nosmooth; we clear those out in might_getNeighbors() for searchmight
 
-        %rid = data.runId(which_rows);
-        %tid = data.trialId(which_rows);
-        for run = runs
-            m = mean(outputs(rid == run & tid > 10, :), 1);
-            disp(m);
+
+        % run classifier n_iter times, to account for randomness in the folds. Average outputs (posteriors) 
+        %
+        o = zeros(size(targets));
+        for iter = 1:n_iter
+            [classifier, outputs, accuracy, stats] = classify_train_helper(method, inputs, targets, runs, trials, [subj], []);
+            o = o + outputs;
         end
-        disp(targets(10:10:90,:));
+        o = o / n_iter;
+        accuracy = classify_get_accuracy(o, targets);
+
+        % use outputs as posteriors in simulations, compare w/ model posterior on test log lik
+        %
+        rid = data.runId(which_rows);
+        tid = data.trialId(which_rows);
+        for run = runs % for each run
+            which_run = data.which_rows & strcmp(data.participant, subject) & data.runId == run;
+            assert(sum(which_run) == metadata.trialsPerRun);
+
+            which_run_test = data.which_rows & ~data.isTrain & ~data.timeout & strcmp(data.participant, subject) & data.runId == run;
+
+            % use the classifier output as posterior to compute the test log likelihood
+            %
+            [train_x, train_k, train_r, test_x, test_k] = convert_run(data, metadata, subject, run);
+            ww_n = simulated.ww_n{which_run & data.trialId == metadata.trainingTrialsPerRun}; % notice we still use the weights from the model
+            %P_n = simulated.P(which_run & data.trialId == metadata.trainingTrialsPerRun,:); <-- sanity; pred_classifier should be same as pred_model in that case
+            P_n = mean(outputs(rid == run & tid > 0, :), 1); % the important part
+            train_results.P_n = P_n;
+            train_results.ww_n = ww_n;
+
+            test_results = model_test(test_x, test_k, train_results, params);
+
+            pred_classifier = test_results.choices(which_run_test(data.which_rows & ~data.isTrain & strcmp(data.participant, subject) & data.runId == run)); % P(choose sick) on the test trials, according to the classifier
+
+
+            % compare with the test log likelihood according to the model
+            %
+            pred_model = simulated.pred(which_run_test); % P(choose sick) on the test trials, according to model
+            X = data.chose_sick(which_run_test); % actual subject choices
+
+            test_liks_classifier = binopdf(X, 1, pred_classifier); 
+            test_liks_model = binopdf(X, 1, pred_model); 
+            assert(numel(test_liks_classifier) == numel(X));
+            assert(numel(test_liks_model) == numel(X));
+
+            avg_loglik_classifier = mean(log(test_liks_classifier));
+            avg_loglik_model = mean(log(test_liks_model));
+
+            liks_classifier = [liks_classifier, avg_loglik_classifier];
+            liks_model = [liks_model, avg_loglik_model];
+        end
 
         acc = [acc, accuracy];
     end
